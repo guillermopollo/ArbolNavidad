7e27bd3a56d864a6d13c777af3100b537abe4ecf
willy <willygamer66@gmail.com>
2023-01-23 13:21:09 +0100

    primer commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+for i in range(1,30,2):
+    print(('^'*i).center(30))
+
+for leg in range(3):
+    print(('|||').center(30))
+    print(('\_____/').center(30))
+    print(' '+30*'-')
+    print('(¨¨¨)¡Feliz Navidad! (¨¨¨)')
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
